ff980c08bd06a8302153be7d35e8bc64d297f87a
alex <AlexKomzzz>
2022-09-10 23:37:50 +0300

    create grpc server

---
+++ pkg/libraryserver/grpcserver.go
@@ -0,0 +1,27 @@
+package libraryserver
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/AlexKomzzz/library-app/pkg/api"
+)
+
+type GRPCserver struct {
+	api.UnimplementedLibraryServer
+	db *sql.DB
+}
+
+func NewGRPCServer(db *sql.DB) *GRPCserver {
+	return &GRPCserver{
+		db: db,
+	}
+}
+
+func (s *GRPCserver) SeachAuthor(ctx context.Context, book *api.Book) (*api.Authors, error) {
+	return &api.Authors{}, nil
+}
+
+func (s *GRPCserver) SeachBook(ctx context.Context, author *api.Author) (*api.Books, error) {
+	return &api.Books{}, nil
+}
